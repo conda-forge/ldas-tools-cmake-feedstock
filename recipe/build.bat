882c2a18a22959ace5901e9f9891dfa51dad74cf
Duncan Macleod <macleoddm@cardiff.ac.uk>
2026-03-20 10:05:05 +0000

    Convert to rattler-build v1

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,21 +1,21 @@
-:: use local build folder
+rem use local build folder
 mkdir build
 cd build
 
-:: configure
+rem configure
 cmake .. ^
 	-G "NMake Makefiles" ^
 	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
 
-:: build
+rem build
 cmake --build .
 if errorlevel 1 exit 1
 
-:: test
+rem test
 ctest -V
 if errorlevel 1 exit 1
 
-:: install
+rem install
 cmake --build . --target install
 if errorlevel 1 exit 1
